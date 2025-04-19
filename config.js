994bf77fa63fbf0b113a4b3fdc5e14fe1a771958
Danydany1234 <danielemura1999@gmail.com>
2025-04-19 02:08:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-// Configura la tua chiave API di Replicate
-const REPLICATE_API_KEY =279d9a7e23764faeafdf4c589992bb32;
+// config.js
+const config = {
+    INTERIOR_DECORATOR_API_KEY:279d9a7e23764faeafdf4c589992bb32  // Sostituisci con la tua chiave API
+};
